bb1afee4e8e8290ac081d102599fdd518ad32e2d
Patrick Armstrong <patricka@uvic.ca>
2013-10-24 14:53:09 -0400

    Swithc to trunk libcloud until 0.14 comes out.

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setupdict['dependency_links'] = [
     'https://github.com/nimbusproject/pidantic/tarball/master#egg=pidantic-0.2',
     'https://github.com/ooici/eeagent/tarball/master#egg=eeagent-0.2',
     'https://github.com/nimbusproject/ceiclient/tarball/master#egg=ceiclient-0.2',
-    'https://github.com/oldpatricka/libcloud/tarball/LIBCLOUD-417#egg=apache-libcloud-0.14.0-dev',
+    'https://github.com/apache/libcloud/tarball/trunk#egg=apache-libcloud-0.14.0-dev',
     'http://sddevrepo.oceanobservatories.org/releases',
     ]
 setupdict['test_suite'] = 'epuharness'
